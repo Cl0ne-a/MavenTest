d6f246538e1b0b9fce1381f24445d7653e34a7e6
Cl0ne-a <63138126+Cl0ne-a@users.noreply.github.com>
2022-07-21 22:37:35 +0300

    Hired pets feeder.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
+import feeder.PetsFeeder;
 import petsplace.PetCollector;
 
 public class Main {
 	public static void main(String[] args) {
-		PetCollector.myHomePets.forEach(pet ->
-				System.out.println(
-						pet.getName() + " " +
-						pet.getType() + " " +
-						pet.getClass()));
+		PetsFeeder feeder = new PetsFeeder();
+		feeder.feedMyPets(PetCollector.myHomePets);
 	}
 }
